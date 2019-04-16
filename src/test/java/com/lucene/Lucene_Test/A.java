dbfd22894efb502f31e2ae02b42efaa05114d032
laxia <1584593657@qq.com>
2019-04-16 10:39:46 +0800

    lucene查询

---
+++ src/test/java/com/lucene/Lucene_Test/A.java
@@ -0,0 +1,9 @@
+package com.lucene.Lucene_Test;
+
+public class A {
+
+	public static void main(String[] args) {
+		String str = "/ss/";
+		System.out.println(str.substring(1, str.length()-1));
+	}
+}
